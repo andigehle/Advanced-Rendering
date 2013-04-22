6f12a2075fb27ddb383b03c1c3e4326e83c2c357
Andreas Gehle <andigehle@gmail.com>
2013-04-22 12:19:58 +0200

    assigment one vis.

--- src/Flow.java
+++ src/Flow.java
@@ -1,5 +1,3 @@
-package exercises;
-
 import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,22 +5,15 @@ import java.util.Scanner;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
-import hilfsklassen.AdvancedTemplate;
 
 /**
- * Vis. assignment 1
- * 19.03.2013
+ * Vis. assignment 1 19.03.2013
  * 
  * @author Andreas Gehle, 6603927
  * 
  * Visualization of a flow data set
- * 
- * - Arrows showing the direction of each water particle flowing
- * - Colors stands for heays/low flow as follow:
- * - - Getting closer to color red means the flow is heavy
- * - - Getting away to color red means the flow is low
  */
-public class Flow extends AdvancedTemplate {
+public class Flow extends JoglTemplate {
 
 	private float[][] flow_data;
 	private int[] flow_spatial_dim = new int[3];
@@ -48,24 +39,11 @@ public class Flow extends AdvancedTemplate {
 		super();
 		setTitle("Visualization of a 2D flow slice");
 
-		setModiFlag(MODI_DEPTH_BUFFERING);
-		// setLightFlag(LIGHT_DIRECTIONAL);
-		// setLightFlag(LIGHT_SHADING);
-
 		// Parse data
 		parseData("data/field2.irreg");
-
-		System.out.println("Min is " + flow_min);
-		System.out.println("Max is " + flow_max);
-		System.out.println("Range is " + (flow_max-flow_min));
-	}
-
-	@Override
-	public void init(GLAutoDrawable drawable) {
-		super.init(drawable);
-
-		GL gl = drawable.getGL();
-		gl.glDisable(GL.GL_LIGHTING);
+		
+		//Zoom out to full view
+		setView_transz(-45);
 	}
 
 	private void parseData(String path) {
@@ -123,27 +101,38 @@ public class Flow extends AdvancedTemplate {
 
 	// ------------------------------------------------------------
 
-	public void displayDraw(GLAutoDrawable drawable) {
+	public void display(GLAutoDrawable drawable) {
+		// get the gl object
 		GL gl = drawable.getGL();
-
-		gl.glEnable(GL.GL_COLOR_MATERIAL);
-
-		// drawCoord(gl, 20);
-
+		// set the erasing color (black)
+		gl.glClearColor(0f, 0f, 0f, 0f);
+		// clear screen with the defined erasing color and depth buffer
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+		
 		gl.glPushMatrix();
 		{
-			// center image on 2d base
-			gl.glTranslatef(-11f, -11f, 0);
 
-			// Maybe use dim for something...border etc. ?
-			gl.glScalef(20, 20, 1);
-			for (int i = 0; i < flow_data.length; i++) {
-				gl.glColor3fv(dataToColor(flow_data[i][6]), 0);
-				drawFlowArrow3D(gl, flow_data[i]);
+			applyMouseTranslation(gl);
+			applyMouseRotation(gl);
+
+			// now do your drawing here!
+			gl.glPushMatrix();
+			{
+
+				// center image on 2d base
+				gl.glTranslatef(-11f, -11f, 0);
+
+				// Maybe use dim for something...border etc. ?
+				gl.glScalef(20, 20, 1);
+				for (int i = 0; i < flow_data.length; i++) {
+					gl.glColor3fv(dataToColor(flow_data[i][6]), 0);
+					drawFlowArrow3D(gl, flow_data[i]);
+				}
+
 			}
+			gl.glPopMatrix();
 		}
 		gl.glPopMatrix();
-
 	}
 
 	private float getLength(float[] coords) {
@@ -151,29 +140,11 @@ public class Flow extends AdvancedTemplate {
 				+ Math.pow((coords[4]), 2) + Math.pow((coords[5]), 2));
 	}
 
-	private void drawFlow(GL gl, float[] coords) {
-		float stroke_length = 0.02f;
-
-		gl.glPushMatrix();
-		{
-			gl.glBegin(GL.GL_LINES);
-			{
-				gl.glVertex3f(coords[0], coords[1], coords[2]);
-				// relative movement
-				gl.glVertex3f(coords[0] + stroke_length * coords[3], coords[1]
-						+ stroke_length * coords[4], coords[2] + stroke_length
-						* coords[5]);
-			}
-			gl.glEnd();
-		}
-		gl.glPopMatrix();
-	}
-
 	private void drawFlowArrow(GL gl, float[] coords) {
 		float stroke_length = 0.02f;
 		float arrow_length = 0.002f;
-		
-		float[] normal = new float[]{-coords[4],coords[3],0};
+
+		float[] normal = new float[] { -coords[4], coords[3], 0 };
 
 		gl.glPushMatrix();
 		{
