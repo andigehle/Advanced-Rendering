eddc6b802cd3ac5bc84e32fdf9426c6990297201
andreasgehle <andreas-gehle@web.de>
2013-05-15 12:35:00 +0200

    bezier texture

--- src/exercises/CopyOfAdR_BezierTemplate.java
+++ src/exercises/CopyOfAdR_BezierTemplate.java
@@ -10,16 +10,22 @@ package exercises;
 
 import hilfsklassen.JoglTemplate;
 
+import java.awt.Component;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.nio.FloatBuffer;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLException;
 
 import com.sun.opengl.util.BufferUtil;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
 
 @SuppressWarnings("serial")
-public class AdR_BezierTemplate extends JoglTemplate {
+public class CopyOfAdR_BezierTemplate extends JoglTemplate {
 	// bezier curve control points (assignment 1)
 	protected final static float CTRL_POINTS[] = { -2.0f, -2.0f, 0.0f, -1.0f,
 			2.0f, 0.0f, 1.0f, -2.0f, 0.0f, 2.0f, 2.0f, 0.0f };
@@ -39,13 +45,15 @@ public class AdR_BezierTemplate extends JoglTemplate {
 	// for loop or glEvalMesh approach
 	protected boolean eval_mesh_approach = false;
 
+	private Texture crateTexture;
+
 	/**
 	 * The main method
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		AdR_BezierTemplate assignment = new AdR_BezierTemplate();
+		CopyOfAdR_BezierTemplate assignment = new CopyOfAdR_BezierTemplate();
 		assignment.setVisible(true);
 	}
 
@@ -58,13 +66,26 @@ public class AdR_BezierTemplate extends JoglTemplate {
 		// calculate normals automatically for evaluators
 		gl.glEnable(GL.GL_AUTO_NORMAL);
 		// use color as material
-		gl.glEnable(GL.GL_COLOR_MATERIAL);
+		//gl.glEnable(GL.GL_COLOR_MATERIAL);
 		// use smooth shading
 		gl.glShadeModel(GL.GL_SMOOTH);
 		// turn light on
 		gl.glEnable(GL.GL_LIGHTING);
 		// use light source 0
 		gl.glEnable(GL.GL_LIGHT0);
+
+		// Loading textures
+		gl.glEnable(GL.GL_TEXTURE_2D);
+		try {
+			File file_crate = new File("assets/textures/crate.png");
+			crateTexture = TextureIO.newTexture(file_crate, false);
+		} catch (GLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -90,48 +111,70 @@ public class AdR_BezierTemplate extends JoglTemplate {
 
 		// copy control points to FloatBuffer
 		FloatBuffer tmpCtrlpointsBuf = BufferUtil
-				.newFloatBuffer(CTRL_POINTS.length);
-		for (int i = 0; i < CTRL_POINTS.length; i++)
-			tmpCtrlpointsBuf.put(CTRL_POINTS[i]);
+				.newFloatBuffer(CTRL_POINTS_3D.length);
+		for (int i = 0; i < CTRL_POINTS_3D.length; i++)
+			tmpCtrlpointsBuf.put(CTRL_POINTS_3D[i]);
 		tmpCtrlpointsBuf.rewind();
+
+		FloatBuffer tmpTexpointsBuf = BufferUtil
+				.newFloatBuffer(TEX_POINTS.length);
+		for (int i = 0; i < TEX_POINTS.length; i++)
+			tmpTexpointsBuf.put(TEX_POINTS[i]);
+		tmpTexpointsBuf.rewind();
 
 		// TODO assign data to evaluator.
 		// Parameters: GL.GL_MAP1_VERTEX_3 specifies the evaluator type, u1, u2,
 		// number of values in each block (should be 3 for vertex3f values),
 		// number of control points, FloatBuffer object
-		gl.glMap1f(GL.GL_MAP1_VERTEX_3, 0, 1, 3, CTRL_POINTS.length / 3,
+		gl.glMap2f(GL.GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4,
 				tmpCtrlpointsBuf);
+		gl.glMap2f(GL.GL_MAP2_TEXTURE_COORD_2, 0, 1, 2, 2, 0, 1, 4, 2,
+				tmpTexpointsBuf);
 
 		// TODO enable evaluator of type GL_MAP1_VERTEX_3
-		gl.glEnable(GL.GL_MAP1_VERTEX_3);
+		gl.glEnable(GL.GL_MAP2_VERTEX_3);
+		gl.glEnable(GL.GL_MAP2_TEXTURE_COORD_2);
 
 		if (!eval_mesh_approach) {
 			// TODO use a for loop and glEvalCoord1f to draw a bezier curve
 			gl.glPushMatrix();
 			{
 				gl.glColor3f(1f, 0f, 0f);
-				gl.glBegin(GL.GL_LINE_STRIP);
-				{
-					for (int i = 0; i <= 100; i++) {
-						gl.glEvalCoord1f((float) i / (float) 100);
+				for (int j = 0; j <= 8; j++) {
+					gl.glBegin(GL.GL_LINE_STRIP);
+					{
+						for (int i = 0; i <= 30; i++)
+							gl.glEvalCoord2f((float) i / 30.0f,
+									(float) j / 8.0f);
 					}
+					gl.glEnd();
+					gl.glBegin(GL.GL_LINE_STRIP);
+					{
+						for (int i = 0; i <= 30; i++)
+							gl.glEvalCoord2f((float) j / 8.0f,
+									(float) i / 30.0f);
+					}
+					gl.glEnd();
 				}
-				gl.glEnd();
 			}
 			gl.glPopMatrix();
 		} else {
 			// TODO use glMapGrid1f and glEvalMesh1 to draw a bezier curve
-			gl.glColor3f(1f, 1f, 0f);
-			gl.glMapGrid1f(100, 0, 1);
-			gl.glEvalMesh1(GL.GL_LINE, 0, 100);
+			gl.glColor3f(0f, 0f, 1f);
+			gl.glMapGrid2f(30, 0, 1, 8, 0, 1);
+			gl.glEvalMesh2(GL.GL_LINE, 0, 30, 0, 8);
+			gl.glColor3f(1f, 0f, 0f);
+			gl.glMapGrid2f(30, 0, 1, 8, 0, 1);
+			gl.glEvalMesh2(GL.GL_FILL, 0, 30, 0, 8);
 		}
 
 		// TODO disable evaluator of type GL_MAP1_VERTEX_3
-		gl.glDisable(GL.GL_MAP1_VERTEX_3);
+		gl.glDisable(GL.GL_MAP2_TEXTURE_COORD_2);
+		gl.glDisable(GL.GL_MAP2_VERTEX_3);
 
 		// draw control points
 		// TODO implement method drawControlPoints(GL gl, float[] ctrlPoints)
-		drawControlPoints(gl, CTRL_POINTS);
+		drawControlPoints(gl, CTRL_POINTS_3D);
 
 		// restore modelview matrix
 		gl.glPopMatrix();
@@ -144,7 +187,7 @@ public class AdR_BezierTemplate extends JoglTemplate {
 			gl.glColor3f(1f, 1f, 0f);
 			gl.glBegin(GL.GL_POINTS);
 			{
-				for (int i = 0; i < 4; i++) {
+				for (int i = 0; i < ctrlPoints.length / 3; i++) {
 					gl.glVertex3fv(ctrlPoints, i * 3);
 				}
 			}
